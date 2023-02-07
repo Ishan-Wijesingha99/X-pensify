3f0395d4d6ddc519bc3cc5c7146a8f9988aa2bac
Ishan <ishanwijes@gmail.com>
2023-02-07 22:01:35 +1100

    added expense form basics

--- frontend/expense-tracker-frontend/src/App.js
+++ frontend/expense-tracker-frontend/src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
+import { ExpenseForm } from "./components/ExpenseForm";
 import { ExpenseList } from "./components/ExpenseList";
 
 
 const App = () => {
   return (
-    <div style={
-      {width: '60%', margin: 'auto'}
-    }
+    <div style={{width: '60%', margin: 'auto'}}
     >
+
+      <h3>New Expenses</h3>
+      <ExpenseForm />
+
+      <hr style={{ border: '1px solid grey' }}/>
+
       <h3>Your Expenses</h3>
       <ExpenseList />
+
     </div>
   )
 }
---
+++ frontend/expense-tracker-frontend/src/components/ExpenseForm.js
@@ -0,0 +1,73 @@
+import React, {useState, useEffect} from "react";
+import { Button, Form, Row, Col } from "react-bootstrap";
+
+
+
+export const ExpenseForm = () => {
+
+  const descriptions = ['Groceries', 'Credit Card Payments', 'Student loans', 'Gas']
+  const [amount, setAmount] = useState(0)
+  const [description, setDescription] = useState(descriptions[0])
+  const [isNewExpense, setIsNewExpense] = useState(true)
+
+  const formSubmit = event => {
+    event.preventDefault()
+
+    if(isNewExpense) {
+      // create new expense
+    } else {
+      // edit expense
+    }
+  }
+
+  return (
+  <Form onSubmit={formSubmit}>
+    <Row>
+      <Col>
+        <Form.Label>Description</Form.Label>
+        <Form.Control
+        as="select"
+        onChange={event => setDescription(event.target.value)}
+        >
+          {descriptions.map(singleDesc => <option>{singleDesc}</option>)}
+        </Form.Control>
+      </Col>
+
+      <Col>
+        <Form.Label>Amount</Form.Label>
+        <Form.Control
+        type="number"
+        step="0.01"
+        placeholder={amount}
+        onChange={event => setAmount(event.target.value)}
+        />
+      </Col>
+
+      {/* this is where we conditionally render the button */}
+      <div className="mt-4">
+        {
+          isNewExpense
+          ?
+          <Button variant="primary" type="submit">
+            Add
+          </Button>
+          :
+          <div>
+            <Button variant="danger">
+              Delete
+            </Button>
+
+            <Button variant="success" type="submit">
+              Save
+            </Button>
+
+            <Button variant="default">
+              Cancel
+            </Button>
+          </div>
+        }
+      </div>
+    </Row>
+  </Form>
+  )
+}
